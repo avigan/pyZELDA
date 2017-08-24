e8d5681f90c08a02dfbdab97e965f15897b800d3
Arthur Vigan <arthur.vigan@lam.fr>
2017-08-24 09:38:40 +0200

    Improved example code
    Added labels and color bar.

--- examples/simple_zelda_analysis.py
+++ examples/simple_zelda_analysis.py
@@ -16,23 +16,26 @@ dark_file = 'SPHERE_GEN_BACKGROUND'
 
 wave = 1.642e-6
 
-clear_pupil, zelda_pupil, center = zelda.read_files(data_path, clear_pupil_files, zelda_pupil_files, dark_file, dim=384)
+clear_pupil, zelda_pupil, center = zelda.read_files(data_path, clear_pupil_files, zelda_pupil_files, dark_file, dim=390)
 
 opd_map = zelda.analyze(clear_pupil, zelda_pupil, wave=wave, pupil_diameter=384)
 
 
-fig = plt.figure(0, figsize=(15, 5))
+fig = plt.figure(0, figsize=(16, 5))
 plt.clf()
 ax = fig.add_subplot(131)
-ax.imshow(clear_pupil, aspect='equal')
-ax.title('Clear pupil')
+ax.imshow(clear_pupil, aspect='equal', vmin=0, vmax=15000)
+ax.set_title('Clear pupil')
 
 ax = fig.add_subplot(132)
-ax.imshow(zelda_pupil.mean(axis=0), aspect='equal')
-ax.title('ZELDA pupil')
+ax.imshow(zelda_pupil.mean(axis=0), aspect='equal', vmin=0, vmax=15000)
+ax.set_title('ZELDA pupil')
 
 ax = fig.add_subplot(133)
-ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-100, vmax=-100)
-ax.title('OPD map')
+cax = ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-150, vmax=150, cmap='magma')
+ax.set_title('OPD map')
+
+cbar = fig.colorbar(cax)
+cbar.set_label('OPD [nm]')
 
 plt.tight_layout()
