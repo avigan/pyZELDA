3f9152f084bac12a19210b52a84febd659e4b50f
Arthur Vigan <arthur.vigan@lam.fr>
2017-08-24 14:55:10 +0200

    Simplified the analyze method
    The ndim == 2 case has been removed. All arrays are now 3D. Added keyword for overwriting input data with the OPD maps to save memory.
    
    Closing #6.

--- examples/simple_zelda_analysis.py
+++ examples/simple_zelda_analysis.py
@@ -18,30 +18,26 @@ zelda_pupil_files = ['SPHERE_ZELDA_PUPIL_CUBE1_NDIT=3', 'SPHERE_ZELDA_PUPIL_CUBE
 dark_file = 'SPHERE_BACKGROUND'
 
 clear_pupil, zelda_pupil, center = zelda.read_files(data_path, clear_pupil_files, zelda_pupil_files, dark_file,
-                                                    dim=390, collapse_clear=False, collapse_zelda=False)
+                                                    dim=390, collapse_clear=True, collapse_zelda=True)
 
-print(clear_pupil.shape)
-print(zelda_pupil.shape)
-
-
-# opd_map = zelda.analyze(clear_pupil, zelda_pupil, wave=wave, pupil_diameter=384)
+opd_map = zelda.analyze(clear_pupil, zelda_pupil, wave=wave, pupil_diameter=384)
 
 
-# fig = plt.figure(0, figsize=(16, 5))
-# plt.clf()
-# ax = fig.add_subplot(131)
-# ax.imshow(clear_pupil, aspect='equal', vmin=0, vmax=15000)
-# ax.set_title('Clear pupil')
+fig = plt.figure(0, figsize=(16, 5))
+plt.clf()
+ax = fig.add_subplot(131)
+ax.imshow(clear_pupil.mean(axis=0), aspect='equal', vmin=0, vmax=15000)
+ax.set_title('Clear pupil')
 
-# ax = fig.add_subplot(132)
-# ax.imshow(zelda_pupil.mean(axis=0), aspect='equal', vmin=0, vmax=15000)
-# ax.set_title('ZELDA pupil')
+ax = fig.add_subplot(132)
+ax.imshow(zelda_pupil.mean(axis=0), aspect='equal')#, vmin=0, vmax=15000)
+ax.set_title('ZELDA pupil')
 
-# ax = fig.add_subplot(133)
-# cax = ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-150, vmax=150, cmap='magma')
-# ax.set_title('OPD map')
+ax = fig.add_subplot(133)
+cax = ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-150, vmax=150, cmap='magma')
+ax.set_title('OPD map')
 
-# cbar = fig.colorbar(cax)
-# cbar.set_label('OPD [nm]')
+cbar = fig.colorbar(cax)
+cbar.set_label('OPD [nm]')
 
-# plt.tight_layout()
+plt.tight_layout()
