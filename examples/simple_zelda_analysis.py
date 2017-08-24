e6d487021a6e704f4d6b7a8ce2d6b3af9bbad1a8
Arthur Vigan <arthur.vigan@lam.fr>
2017-08-24 14:22:53 +0200

    Multiple improvements to manage different number of NDITs for ZELDA and clear pupil images
    Closing issue #4.
    Closing issue #3.

--- examples/simple_zelda_analysis.py
+++ examples/simple_zelda_analysis.py
@@ -2,7 +2,8 @@ import sys
 import os
 import matplotlib.pyplot as plt
 
-path = '/Users/mndiaye/Dropbox/python/zelda/pyZELDA/'
+# path = '/Users/mndiaye/Dropbox/python/zelda/pyZELDA/'
+path = '/Users/avigan/Work/GitHub/pyZELDA/'
 if path not in sys.path:
     sys.path.append(path)
 import pyzelda.zelda as zelda
@@ -10,32 +11,37 @@ import pyzelda.zelda as zelda
 
 data_path = os.path.join(path, 'data/')
 
-clear_pupil_files = ['SPHERE_GEN_CLEAR_PUPIL_1', 'SPHERE_GEN_CLEAR_PUPIL_2']
-zelda_pupil_files = ['SPHERE_GEN_ZELDA_PUPIL_1', 'SPHERE_GEN_ZELDA_PUPIL_2']
-dark_file = 'SPHERE_GEN_BACKGROUND'
-
 wave = 1.642e-6
 
-clear_pupil, zelda_pupil, center = zelda.read_files(data_path, clear_pupil_files, zelda_pupil_files, dark_file, dim=390)
+clear_pupil_files = ['SPHERE_CLEAR_PUPIL_CUBE1_NDIT=3', 'SPHERE_CLEAR_PUPIL_CUBE1_NDIT=3']
+zelda_pupil_files = ['SPHERE_ZELDA_PUPIL_CUBE1_NDIT=3', 'SPHERE_ZELDA_PUPIL_CUBE2_NDIT=3']
+dark_file = 'SPHERE_BACKGROUND'
 
-opd_map = zelda.analyze(clear_pupil, zelda_pupil, wave=wave, pupil_diameter=384)
+clear_pupil, zelda_pupil, center = zelda.read_files(data_path, clear_pupil_files, zelda_pupil_files, dark_file,
+                                                    dim=390, collapse_clear=False, collapse_zelda=False)
+
+print(clear_pupil.shape)
+print(zelda_pupil.shape)
 
 
-fig = plt.figure(0, figsize=(16, 5))
-plt.clf()
-ax = fig.add_subplot(131)
-ax.imshow(clear_pupil, aspect='equal', vmin=0, vmax=15000)
-ax.set_title('Clear pupil')
+# opd_map = zelda.analyze(clear_pupil, zelda_pupil, wave=wave, pupil_diameter=384)
 
-ax = fig.add_subplot(132)
-ax.imshow(zelda_pupil.mean(axis=0), aspect='equal', vmin=0, vmax=15000)
-ax.set_title('ZELDA pupil')
 
-ax = fig.add_subplot(133)
-cax = ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-150, vmax=150, cmap='magma')
-ax.set_title('OPD map')
+# fig = plt.figure(0, figsize=(16, 5))
+# plt.clf()
+# ax = fig.add_subplot(131)
+# ax.imshow(clear_pupil, aspect='equal', vmin=0, vmax=15000)
+# ax.set_title('Clear pupil')
 
-cbar = fig.colorbar(cax)
-cbar.set_label('OPD [nm]')
+# ax = fig.add_subplot(132)
+# ax.imshow(zelda_pupil.mean(axis=0), aspect='equal', vmin=0, vmax=15000)
+# ax.set_title('ZELDA pupil')
 
-plt.tight_layout()
+# ax = fig.add_subplot(133)
+# cax = ax.imshow(opd_map.mean(axis=0), aspect='equal', vmin=-150, vmax=150, cmap='magma')
+# ax.set_title('OPD map')
+
+# cbar = fig.colorbar(cax)
+# cbar.set_label('OPD [nm]')
+
+# plt.tight_layout()
