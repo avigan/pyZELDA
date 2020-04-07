5601754e276c395512384cf52d1fa8eb741675a7
Arthur Vigan <arthur.vigan@lam.fr>
2020-04-07 15:37:52 +0200

    Minor fix on setup string

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='pyZELDA',
+    name='pyzelda',
     version='1.1',
     description='Zernike wavefront sensor analysis and simulation tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/avigan/pyZELDA',
-    author='Arthur Vigan & Mamadou N\'Diaye',
+    author="Arthur Vigan & Mamadou N'Diaye",
     author_email='arthur.vigan@lam.fr, mamadou.ndiaye@oca.eu',
     license='MIT',
     classifiers=[
