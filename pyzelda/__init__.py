c09b75450349962c77c0421a044e36dc1714ce41
Arthur Vigan <arthur.vigan@lam.fr>
2021-04-01 19:48:17 +0200

    Bump version number to 1.2, update README

--- pyzelda/__init__.py
+++ pyzelda/__init__.py
@@ -1,7 +1,7 @@
 __author__    = "Arthur Vigan, Mamadou N'Diaye"
 __copyright__ = "Copyright (C) 2017-2020 Arthur Vigan, Mamadou N'Diaye"
 __license__   = 'MIT'
-__version__   = '1.1'
+__version__   = '1.2'
 
 # direct access to sensor object
 from .zelda import Sensor
